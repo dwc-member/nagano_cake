f94f33f9a87dca2d1fbfaa8adf3f0b4ce03ae634
shioriyokoo <83577622+shioriyokoo@users.noreply.github.com>
2023-01-17 06:38:20 +0000

    Add Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 会員側のルーティング設定
+  get 'items' => 'public/items#index'
+  # 管理者側のルーティング設定
+  namespace :admin do
+    resources :items
+  end
 end
