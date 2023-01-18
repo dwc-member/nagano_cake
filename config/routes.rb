2c304d26f78141c4ffce0bb8d898677f2cf1b39f
shioriyokoo <83577622+shioriyokoo@users.noreply.github.com>
2023-01-18 09:14:31 +0000

    Add Devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # 会員側のルーティング設定
   get 'items' => 'public/items#index'
+  root to: 'public/homes#top'
   # 管理者側のルーティング設定
   namespace :admin do
     resources :items
+    get '/' => 'homes#top'
   end
 end
