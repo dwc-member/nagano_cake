fbabd795aac5a32110da7a4d72985b04cda69762
shioriyokoo <83577622+shioriyokoo@users.noreply.github.com>
2023-01-21 07:27:44 +0000

    Rename アプリケーション名

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # 会員側のルーティング設定
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
   get 'items' => 'public/items#index'
   root to: 'public/homes#top'
-  # 管理者側のルーティング設定
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   namespace :admin do
     resources :items, only: [:new, :create, :index, :show]
     root to: 'admin/homes#top'
